c2b54b68fc9cb0024ace4dbc0bc6ec4d7ee07bfd
tsanie <tsorgy@gmail.com>
2015-06-03 11:15:31 +0800

    initial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace this_is_a_test
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			var assembly = Assembly.GetExecutingAssembly();
+
+			Console.WriteLine( assembly.GetName().Version );
+			Console.WriteLine( assembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version );
+			Console.WriteLine( assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion );
+
+			Console.Write( "done." );
+			Console.ReadKey( true );
+		}
+	}
+}
